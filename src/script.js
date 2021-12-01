363e96680a0bf43b0d7d5b8f600b35c82612c01c
AlirezaRezei <cavalier.boy76@gmail.com>
2021-12-01 16:08:29 +0330

    add sphere geometry

--- src/script.js
+++ src/script.js
@@ -3,6 +3,10 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 
+//Loading
+const textureLoading = new THREE.TextureLoader()
+const normalTexture = textureLoading.load('/map.png')
+
 // Debug
 const gui = new dat.GUI()
 
@@ -13,12 +17,15 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+const geometry =new THREE.SphereGeometry( 0.7, 64, 64 );
 
 // Materials
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+const material = new THREE.MeshStandardMaterial()
+material.metalness = 0.5;
+material.roughness =0
+material.normalMap = normalTexture;
+material.color = new THREE.Color(0x292929)
 
 // Mesh
 const sphere = new THREE.Mesh(geometry,material)
@@ -26,11 +33,22 @@ scene.add(sphere)
 
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(1,1,1)
+pointLight.intensity = 0.5
 scene.add(pointLight)
+
+const pointLight2 = new THREE.PointLight(0xff0000)
+pointLight2.position.set(2,3,4)
+pointLight2.intensity = 2
+scene.add(pointLight2)
+
+gui.add(pointLight2,'intensity')
+
+gui.add(pointLight2.position,'y')
+gui.add(pointLight2.position,'x')
+gui.add(pointLight2.position,'z')
+
 
 /**
  * Sizes
@@ -40,20 +58,20 @@ const sizes = {
     height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+// window.addEventListener('resize', () =>
+// {
+//     // Update sizes
+//     sizes.width = window.innerWidth
+//     sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+//     // Update camera
+//     camera.aspect = sizes.width / sizes.height
+//     camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+//     // Update renderer
+//     renderer.setSize(sizes.width, sizes.height)
+//     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+// })
 
 /**
  * Camera
@@ -73,7 +91,8 @@ scene.add(camera)
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    alpha:true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
